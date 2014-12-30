3f29511087a17c3fa1f7f4afe01da9f2ebecb4a6
Robert Biggs <robert.biggs@sourcebits.com>
2014-12-30 10:11:10 -0800

    Fixed some data errors.

--- data/bestWines.js
+++ data/bestWines.js
@@ -6,73 +6,85 @@
         "id": "002",
         "name": "Leonardini Merlot",
         "winery": "Whitehall",
-        "year": "2010"
+        "year": "2010",
+        "location": "Whitehall+Lane+Winery"
       },  
       {
         "id": "005",
         "name": "Cabernet Sauvignon",
         "winery": "Benziger",
-        "year": "2011"
+        "year": "2011",
+        "location": "Benziger+Family+Winery"
       },
       {
         "id": "009",
         "name": "Signaterra Pinot Noir",
         "winery": "Benziger",
-        "year": "2012"
+        "year": "2012",
+        "location": "Benziger+Family+Winery"
       },
       {
-        "id": "205",
+        "id": "202",
         "name": "Estate Pinot Noir",
         "winery": "Monticello",
-        "year": "2011"
+        "year": "2011",
+        "location": "Monticello+Vineyards"
       },
       {
-        "id": "199",
+        "id": "196",
         "name": "Petite Sirah",
         "winery": "Rutherford Grove",
-        "year": "2008"
+        "year": "2008",
+        "location": "Rutherford+Grove+Winery"
       },
       {
-        "id": "186",
+        "id": "183",
         "name": "Estate Syrah",
         "winery": "Spring Mountain",
-        "year": "2009"
+        "year": "2009",
+        "location": "Spring+Mountain+Vineyard"
       },
       {
-        "id": "184",
+        "id": "181",
         "name": "Cabernet Sauvignon",
         "winery": "Spring Mountain",
-        "year": "2010"
+        "year": "2010",
+        "location": "Spring+Mountain+Vineyard"
       },
       {
-        "id": "179",
+        "id": "177",
         "name": "Merlot",
         "winery": "St. Clement",
-        "year": "2009"
+        "year": "2009",
+        "location": "St.+Clement+Vineyards"
       },
       {
-        "id": "174",
+        "id": "172",
         "name": "Zinfandel",
         "winery": "Summit Lake",
-        "year": "2009"
+        "year": "2009",
+        "location": "Summit+Lake+Vineyards"
       },
       {
-        "id": "171",
+        "id": "169",
         "name": "Cabernet Sauvignon",
         "winery": "Tedeschi Family",
-        "year": "2006"
+        "year": "2006",
+        "location": "Tedeschi+Family+Winery"
       },
       {
-        "id": "128",
+        "id": "126",
         "name": "Syrah",
         "winery": "Delectus",
-        "year": "2008"
+        "year": "2008",
+        "location": "578+Gateway+Drive+Napa+CA+94558"
       },
       {
-        "id": "115",
-        "name": "Zinfandel",
+        "id": "113",
+        "name": "Montelena Zinfandel",
         "winery": "Chateau Montelena",
-        "year": "2011"
+        "year": "2011",
+        "location": "Chateau+Montelena+Winery"
       }
     ]
   },
@@ -83,73 +95,85 @@
         "id": "083",
         "name": "Pinot Grigio",
         "winery": "Benessere",
-        "year": "2012"
+        "year": "2012",
+        "location": "Benessere+Vineyards"
       },
       {
         "id": "087",
         "name": "Sorelle Chardonnay",
         "winery": "Baldacci Family",
-        "year": "2012"
+        "year": "2012",
+        "location": "Baldacci+Family+Vineyards"
       },
       {
-        "id": "136",
+        "id": "134",
         "name": "Chardonnay",
         "winery": "Duckhorn",
-        "year": "2012"
+        "year": "2012",
+        "location": "Duckhorn+Vineyards"
       },
       {
-        "id": "139",
+        "id": "135",
         "name": "Sauvignon Blanc",
         "winery": "Duckhorn",
-        "year": "2013"
+        "year": "2013",
+        "location": "Duckhorn+Vineyards"
       },
       {
-        "id": "147",
+        "id": "145",
         "name": "Chardonnay",
         "winery": "Franciscan",
-        "year": "2013"
+        "year": "2013",
+        "location": "Franciscan+Estate"
       },
       {
-        "id": "150",
+        "id": "148",
         "name": "Sauvignon Blanc",
         "winery": "Jericho Canyon",
-        "year": "2013"
+        "year": "2013",
+        "location": "Jericho+Canyon+Vineyard"
       },
       {
-        "id": "159",
+        "id": "157",
         "name": "Sauvignon Blanc",
         "winery": "Kenefick Ranch",
-        "year": "2013"
+        "year": "2013",
+        "location": "Kenefick+Ranch+Winery"
       },
       {
-        "id": "168",
+        "id": "166",
         "name": "Chardonnay",
         "winery": "Tedeschi Family",
-        "year": "2012"
+        "year": "2012",
+        "location": "Tedeschi+Family+Winery"
+      },
+      {
+        "id": "180",
+        "name": "Sauvignon Blanc",
+        "winery": "St. Clement",
+        "year": "2012",
+        "location": "St.+Clement+Vineyards"
       },
       {
         "id": "182",
         "name": "Sauvignon Blanc",
-        "winery": "St. Clement",
-        "year": "2012"
-      },
-      {
-        "id": "185",
-        "name": "Sauvignon Blanc",
         "winery": "Spring Mountain",
-        "year": "2012"
+        "year": "2012",
+        "location": "Spring+Mountain+Vineyard"
       },
       {
-        "id": "191",
+        "id": "188",
         "name": "Chardonnay",
         "winery": "Robert Mondavi",
-        "year": "2011"
+        "year": "2011",
+        "location": "Robert+Mondavi+Winery"
       },
       {
-        "id": "207",
+        "id": "204",
         "name": "Estate Chardonnay",
         "winery": "Monticello",
-        "year": "2010"
+        "year": "2010",
+        "location": "Monticello+Vineyards"
       }
     ]
   }
